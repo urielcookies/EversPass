27ee8209d7a86d02bd209e400a87721405410aaf
Ever Garcia <urielcookies@outlook.com>
2023-09-28 21:48:45 -0600

    Navigation Added to edit form page

--- src/Components/PassCodeContentEditor/PassCodeContentEditor.tsx
+++ src/Components/PassCodeContentEditor/PassCodeContentEditor.tsx
@@ -1,17 +1,128 @@
-import React, { FC } from 'react'
-import { Text } from 'react-native-paper'
-import { View } from 'react-native'
-import { NavigationProp, RouteProp } from '@react-navigation/native';
+import React, { FC, useState } from 'react';
+import { ScrollView, View, TouchableWithoutFeedback, NativeScrollEvent, NativeSyntheticEvent, StyleSheet } from 'react-native';
+import { Text, useTheme } from 'react-native-paper';
+import { NavigationProp, RouteProp, useNavigation } from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { MD3Colors } from "react-native-paper/lib/typescript/types";
+
+import ViewWrapper from '../ViewWrapper/ViewWrapper';
 
 const PassCodeContentEditor: FC<PassCodeContentProps> = (props) => {
   const { data } = props.route.params;
-  console.log('data-->>', data);
+
+  const navigation = useNavigation<Nav>();
+
+  const { colors } = useTheme();
+  const styles = themeStyle(colors);
+
+  const [navBarStyles, setNavBarStyles] = useState(styles.navIcons);
+  const [showTitle, setShowTitle] = useState(false);
+
+  const gotoPassCodeConentStackScreen = () =>
+    navigation.goBack();
+
+  const gotoEditorStackScreen = () => {
+		navigation.navigate("Home");
+	};
+
+  const onScrollHandler = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    if (event.nativeEvent.contentOffset.y > 200) {
+      setShowTitle(true);
+      setNavBarStyles({
+        ...styles.navIcons,
+        ...{
+            backgroundColor: colors.onPrimary,
+            borderBottomColor: colors.onSecondaryContainer,
+            borderBottomWidth: 2,
+            // opacity: 0.5/* Change the opacity value as desired */
+          }
+      })
+    } else {
+      setShowTitle(false);
+      setNavBarStyles(styles.navIcons)
+    }
+  }
   return (
-    <View>
-      <Text>{data.title}</Text>
-    </View>
+    <ViewWrapper notchProtection>
+      <ScrollView stickyHeaderIndices={[ 0 ]} onScroll={onScrollHandler}>
+        <View>
+          <View style={navBarStyles}>
+            <TouchableWithoutFeedback onPress={gotoPassCodeConentStackScreen}>
+              <View style={styles.back}>
+                <MaterialCommunityIcons name="arrow-left" size={30} color={colors.onSecondaryContainer} />
+              </View>
+            </TouchableWithoutFeedback>
+
+            {showTitle && (
+              <View>
+                <Text variant="headlineLarge">{data.title}</Text>
+              </View>
+            )}
+
+            <TouchableWithoutFeedback onPress={gotoEditorStackScreen}>
+              <View style={styles.edit}>
+                <MaterialCommunityIcons name="pencil-outline" size={30} color={colors.onSecondaryContainer} />
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+        </View>
+
+        <View style={styles.title}>
+          <Text variant="headlineLarge">{data.title}</Text>
+        </View>
+      </ScrollView>
+    </ViewWrapper>
   )
 }
+
+const themeStyle = (colors: MD3Colors) => StyleSheet.create({
+	navIcons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderRadius: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 7.5,
+	},
+  back: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 50,
+    width: '15%',
+    justifyContent: 'flex-start',
+  },
+  edit: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 50,
+    width: '15%',
+    justifyContent: 'flex-end',
+    
+  },
+  title: {
+    height: 40,
+    borderRadius: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 7.5,
+    marginBottom: 7.5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  content: {
+    backgroundColor: colors.onPrimary,
+    borderRadius: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 7.5,
+    marginBottom: 7.5,
+  },
+  icon: {
+    color: colors.onSecondaryContainer,
+    fontSize: 20,
+  }
+});
 
 type RootStackParamList = {
   // Home: undefined;
@@ -24,7 +135,8 @@ interface PassCodeContentProps {
 }
 
 type Nav = {
-  navigate: (value: string) => void;
+  navigate: (value: string, data?: { data: PassCodeProps } | { data: PassCodeProps[] }) => void;
+  goBack: () => void;
 }
 
 export interface PassCodeProps {
@@ -55,4 +167,4 @@ interface CustomField {
   [key: string]: string;
 }
 
-export default PassCodeContentEditor
+export default PassCodeContentEditor;
