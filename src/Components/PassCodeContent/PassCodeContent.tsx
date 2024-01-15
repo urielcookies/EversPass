c876a2e2b5fd3ae9e792acacf65cc570990493b8
Ever Uriel Garcia <urielcookies@outlook.com>
2024-01-14 22:01:52 -0700

    Linting done

--- src/Components/PassCodeContent/PassCodeContent.tsx
+++ src/Components/PassCodeContent/PassCodeContent.tsx
@@ -1,16 +1,27 @@
-import { Image, NativeScrollEvent, NativeSyntheticEvent, ScrollView, StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
-import { useNavigation, NavigationProp, RouteProp } from '@react-navigation/native';
+import {
+  Image,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  ScrollView,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import {
+  useNavigation,
+  NavigationProp,
+  RouteProp,
+} from '@react-navigation/native';
 import { List, Text, useTheme } from 'react-native-paper';
-import { MD3Colors } from "react-native-paper/lib/typescript/types";
+import { MD3Colors } from 'react-native-paper/lib/typescript/types';
 import ViewWrapper from '../ViewWrapper/ViewWrapper';
 import { FC, useState } from 'react';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import PassCodeFields from './PassCodeFields';
 import { isEmpty, isUndefined, map } from 'lodash';
 
-
-const PassCodeContent: FC<PassCodeContentProps> = (props) => {
-	const { data } = props.route.params;
+const PassCodeContent: FC<PassCodeContentProps> = props => {
+  const { data } = props.route.params;
 
   const navigation = useNavigation<Nav>();
 
@@ -20,21 +31,21 @@ const PassCodeContent: FC<PassCodeContentProps> = (props) => {
     PASSWORD: 210,
     CREDITCARD: 350,
     PERSONALINFO: 280,
-    SECURENOTE: 0
+    SECURENOTE: 0,
   };
 
-	const styles = themeStyle(colors, numberofFields[data.securityType]);
+  const styles = themeStyle(colors, numberofFields[data.securityType]);
 
   const [navBarStyles, setNavBarStyles] = useState(styles.navIcons);
   const [showTitle, setShowTitle] = useState(false);
 
   const gotoTestStackScreen = () => {
-		navigation.navigate("Home");
-	};
+    navigation.navigate('Home');
+  };
 
   const gotoEditorStackScreen = () => {
-		navigation.navigate("PassCodeContentEditor", { data });
-	};
+    navigation.navigate('PassCodeContentEditor', { data });
+  };
 
   const onScrollHandler = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
     if (event.nativeEvent.contentOffset.y > 200) {
@@ -42,26 +53,33 @@ const PassCodeContent: FC<PassCodeContentProps> = (props) => {
       setNavBarStyles({
         ...styles.navIcons,
         ...{
-            backgroundColor: colors.onPrimary,
-            borderBottomColor: colors.onSecondaryContainer,
-            borderBottomWidth: 2,
-            // opacity: 0.5/* Change the opacity value as desired */
-          }
-      })
+          backgroundColor: colors.onPrimary,
+          borderBottomColor: colors.onSecondaryContainer,
+          borderBottomWidth: 2,
+          // opacity: 0.5/* Change the opacity value as desired */
+        },
+      });
     } else {
       setShowTitle(false);
-      setNavBarStyles(styles.navIcons)
+      setNavBarStyles(styles.navIcons);
     }
-  }
+  };
 
   return (
     <ViewWrapper notchProtection>
-      <ScrollView stickyHeaderIndices={[ 0 ]} onScroll={onScrollHandler}>
+      <ScrollView
+        stickyHeaderIndices={[0]}
+        scrollEventThrottle={16}
+        onScroll={onScrollHandler}>
         <View>
           <View style={navBarStyles}>
             <TouchableWithoutFeedback onPress={gotoTestStackScreen}>
               <View style={styles.back}>
-                <MaterialCommunityIcons name="arrow-left" size={30} color={colors.onSecondaryContainer} />
+                <MaterialCommunityIcons
+                  name="arrow-left"
+                  size={30}
+                  color={colors.onSecondaryContainer}
+                />
               </View>
             </TouchableWithoutFeedback>
 
@@ -73,17 +91,22 @@ const PassCodeContent: FC<PassCodeContentProps> = (props) => {
 
             <TouchableWithoutFeedback onPress={gotoEditorStackScreen}>
               <View style={styles.edit}>
-                <MaterialCommunityIcons name="pencil-outline" size={30} color={colors.onSecondaryContainer} />
+                <MaterialCommunityIcons
+                  name="pencil-outline"
+                  size={30}
+                  color={colors.onSecondaryContainer}
+                />
               </View>
             </TouchableWithoutFeedback>
           </View>
-      </View>
+        </View>
 
         <View style={styles.logoIcon}>
           <Image
             style={styles.logoIcon.logo}
             source={require('../../Assets/amex.jpeg')}
-            defaultSource={require('../../Assets/amex.jpeg')} />
+            defaultSource={require('../../Assets/amex.jpeg')}
+          />
         </View>
 
         <View style={styles.title}>
@@ -94,7 +117,11 @@ const PassCodeContent: FC<PassCodeContentProps> = (props) => {
           <View>
             <TouchableWithoutFeedback onPress={gotoTestStackScreen}>
               <View style={styles.actionButton}>
-                <MaterialCommunityIcons name="attachment" size={30} color={colors.onSecondaryContainer} />
+                <MaterialCommunityIcons
+                  name="attachment"
+                  size={30}
+                  color={colors.onSecondaryContainer}
+                />
                 <Text>Attach File</Text>
               </View>
             </TouchableWithoutFeedback>
@@ -102,7 +129,11 @@ const PassCodeContent: FC<PassCodeContentProps> = (props) => {
           <View>
             <TouchableWithoutFeedback onPress={gotoTestStackScreen}>
               <View style={styles.actionButton}>
-                <MaterialCommunityIcons name="account-plus-outline" size={30} color={colors.onSecondaryContainer} />
+                <MaterialCommunityIcons
+                  name="account-plus-outline"
+                  size={30}
+                  color={colors.onSecondaryContainer}
+                />
                 <Text>Share</Text>
               </View>
             </TouchableWithoutFeedback>
@@ -110,7 +141,11 @@ const PassCodeContent: FC<PassCodeContentProps> = (props) => {
           <View>
             <TouchableWithoutFeedback onPress={gotoTestStackScreen}>
               <View style={styles.actionButton}>
-                <MaterialCommunityIcons name="dots-vertical" size={30} color={colors.onSecondaryContainer} />
+                <MaterialCommunityIcons
+                  name="dots-vertical"
+                  size={30}
+                  color={colors.onSecondaryContainer}
+                />
                 <Text>More</Text>
               </View>
             </TouchableWithoutFeedback>
@@ -118,26 +153,30 @@ const PassCodeContent: FC<PassCodeContentProps> = (props) => {
         </View>
 
         <View style={styles.content}>
-          <PassCodeFields data={data}/>
+          <PassCodeFields data={data} />
         </View>
 
-        {Boolean(data.passData.website) && <View style={styles.webApp}>
-          <List.Item
-            titleStyle={{ fontSize: 15, color: 'grey' }}
-            descriptionStyle={{ fontSize: 15 }}
-            title="Website or App Name"
-            description={data.passData.website} />
-        </View>
-        }
+        {Boolean(data.passData.website) && (
+          <View style={styles.webApp}>
+            <List.Item
+              titleStyle={{ fontSize: 15, color: 'grey' }}
+              descriptionStyle={{ fontSize: 15 }}
+              title="Website or App Name"
+              description={data.passData.website}
+            />
+          </View>
+        )}
 
         {!isEmpty(data.passData.customFields) && (
           <View style={styles.customFields}>
-            {map(data.passData.customFields, (element) => (
+            {map(data.passData.customFields, element => (
               <List.Item
+                key={element.name}
                 titleStyle={{ fontSize: 15, color: 'grey' }}
                 descriptionStyle={{ fontSize: 15 }}
                 title={element.name}
-                description={element.value} />
+                description={element.value}
+              />
             ))}
           </View>
         )}
@@ -148,159 +187,159 @@ const PassCodeContent: FC<PassCodeContentProps> = (props) => {
               descriptionStyle={{ fontSize: 15 }}
               descriptionNumberOfLines={data.passData.note.length * 100}
               title="Note"
-              description={data.passData.note} />
+              description={data.passData.note}
+            />
           </View>
         )}
       </ScrollView>
     </ViewWrapper>
-	);
-}
+  );
+};
 
-const themeStyle = (colors: MD3Colors, totalFields: number) => StyleSheet.create({
-	navIcons: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderRadius: 10,
-    marginLeft: 10,
-    marginRight: 10,
-    marginBottom: 7.5,
-	},
-  back: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    height: 50,
-    width: '15%',
-    justifyContent: 'flex-start',
-  },
-  edit: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    height: 50,
-    width: '15%',
-    justifyContent: 'flex-end',
-    
-  },
-  logoIcon: {
-		// flex: 3,
-    // height: '20%',
-    height: 100,
-    // backgroundColor: 'blue',
-    // backgroundColor: colors.onPrimary,
-    borderRadius: 10,
-    marginLeft: 10,
-    marginRight: 10,
-    marginTop: 7.5,
-    marginBottom: 7.5,
-    alignItems: 'center',
-    justifyContent: 'center',
-    logo: {
-      width: undefined,
-      height: '100%',
-      aspectRatio: 1,
+const themeStyle = (colors: MD3Colors, totalFields: number) =>
+  StyleSheet.create({
+    navIcons: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
       borderRadius: 10,
-    }
-  },
-  title: {
-		// flex: 2,
-    // height: '10%',
-    height: 40,
-    // backgroundColor: 'yellow',
-    borderRadius: 10,
-    marginLeft: 10,
-    marginRight: 10,
-    marginTop: 7.5,
-    marginBottom: 7.5,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  actionButtons: {
-		// flex: 2.8,
-    // height: '15%',
-    height: 80,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    // backgroundColor: colors.onPrimary,
-    borderRadius: 10,
-    marginLeft: 10,
-    marginRight: 10,
-    marginTop: 7.5,
-    marginBottom: 7.5,
-  },
-  actionButton: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: colors.onPrimary,
-    height: '100%',
-    width: 125,
-    fontSize: '20px',
-    borderRadius: 10,
-    gap: 10,
-  },
-  content: {
-		// flex: totalFields,
-    // height: '60%',
-    height: totalFields,
-    // backgroundColor: 'pink',
-    backgroundColor: colors.onPrimary,
-    borderRadius: 10,
-    marginLeft: 10,
-    marginRight: 10,
-    marginTop: 7.5,
-    marginBottom: 7.5,
-  },
-  webApp: {
-		// flex: 2,
-    // height: '15%',
-    // backgroundColor: 'green',
-    backgroundColor: colors.onPrimary,
-    borderRadius: 10,
-    marginLeft: 10,
-    marginRight: 10,
-    marginTop: 7.5,
-    marginBottom: 7.5,
-  },
-  customFields: {
-		// flex: 2,
-    // height: '15%',
-    backgroundColor: colors.onPrimary,
-    borderRadius: 10,
-    marginLeft: 10,
-    marginRight: 10,
-    marginTop: 7.5,
-    marginBottom: 7.5,
-  },
-  note: {
-		// flex: 2,
-    // height: '15%',
-    // backgroundColor: 'green',
-    backgroundColor: colors.onPrimary,
-    borderRadius: 10,
-    marginLeft: 10,
-    marginRight: 10,
-    marginTop: 7.5,
-    marginBottom: 7.5,
-  },
-  icon: {
-    color: colors.onSecondaryContainer,
-    fontSize: 20,
-  }
-});
+      marginLeft: 10,
+      marginRight: 10,
+      marginBottom: 7.5,
+    },
+    back: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      height: 50,
+      width: '15%',
+      justifyContent: 'flex-start',
+    },
+    edit: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      height: 50,
+      width: '15%',
+      justifyContent: 'flex-end',
+    },
+    logoIcon: {
+      // flex: 3,
+      // height: '20%',
+      height: 100,
+      // backgroundColor: 'blue',
+      // backgroundColor: colors.onPrimary,
+      borderRadius: 10,
+      marginLeft: 10,
+      marginRight: 10,
+      marginTop: 7.5,
+      marginBottom: 7.5,
+      alignItems: 'center',
+      justifyContent: 'center',
+      logo: {
+        width: undefined,
+        height: '100%',
+        aspectRatio: 1,
+        borderRadius: 10,
+      },
+    },
+    title: {
+      // flex: 2,
+      // height: '10%',
+      height: 40,
+      // backgroundColor: 'yellow',
+      borderRadius: 10,
+      marginLeft: 10,
+      marginRight: 10,
+      marginTop: 7.5,
+      marginBottom: 7.5,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    actionButtons: {
+      // flex: 2.8,
+      // height: '15%',
+      height: 80,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      // backgroundColor: colors.onPrimary,
+      borderRadius: 10,
+      marginLeft: 10,
+      marginRight: 10,
+      marginTop: 7.5,
+      marginBottom: 7.5,
+    },
+    actionButton: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: colors.onPrimary,
+      height: '100%',
+      width: 125,
+      fontSize: '20px',
+      borderRadius: 10,
+      gap: 10,
+    },
+    content: {
+      // flex: totalFields,
+      // height: '60%',
+      height: totalFields,
+      // backgroundColor: 'pink',
+      backgroundColor: colors.onPrimary,
+      borderRadius: 10,
+      marginLeft: 10,
+      marginRight: 10,
+      marginTop: 7.5,
+      marginBottom: 7.5,
+    },
+    webApp: {
+      // flex: 2,
+      // height: '15%',
+      // backgroundColor: 'green',
+      backgroundColor: colors.onPrimary,
+      borderRadius: 10,
+      marginLeft: 10,
+      marginRight: 10,
+      marginTop: 7.5,
+      marginBottom: 7.5,
+    },
+    customFields: {
+      // flex: 2,
+      // height: '15%',
+      backgroundColor: colors.onPrimary,
+      borderRadius: 10,
+      marginLeft: 10,
+      marginRight: 10,
+      marginTop: 7.5,
+      marginBottom: 7.5,
+    },
+    note: {
+      // flex: 2,
+      // height: '15%',
+      // backgroundColor: 'green',
+      backgroundColor: colors.onPrimary,
+      borderRadius: 10,
+      marginLeft: 10,
+      marginRight: 10,
+      marginTop: 7.5,
+      marginBottom: 7.5,
+    },
+    icon: {
+      color: colors.onSecondaryContainer,
+      fontSize: 20,
+    },
+  });
 
 type RootStackParamList = {
   // Home: undefined;
   PassCodeContent: { data: PassCodeProps };
-}
-
+};
 
 interface PassCodeContentProps {
-	navigation: NavigationProp<Nav>;
-	route: RouteProp<RootStackParamList, 'PassCodeContent'>;
+  navigation: NavigationProp<Nav>;
+  route: RouteProp<RootStackParamList, 'PassCodeContent'>;
 }
 
 type Nav = {
   navigate: (value: string, data?: { data: PassCodeProps }) => void;
-}
+};
 
 export interface PassCodeProps {
   id: number;
