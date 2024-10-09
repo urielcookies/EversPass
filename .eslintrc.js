41f78901ef1d0fbe6369253e9d7407b82adcd2ba
Ever Uriel Garcia <urielcookies@outlook.com>
2024-10-09 13:24:07 -0600

    Refactor project configuration files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  // "linebreak-style": ["error", "unix", "windows"]
-  rules: {
-    // whitespace: [true, 'check-module'],
-    'object-curly-spacing': ['error', 'always'],
-    'react/react-in-jsx-scope': 0,
-    'max-len': ['error', { code: 100 }],
-    'react/no-unstable-nested-components': 0,
-  },
+  extends: '@react-native',
+    // "linebreak-style": ["error", "unix", "windows"]
+    rules: {
+      // whitespace: [true, 'check-module'],
+      'object-curly-spacing': ['error', 'always'],
+      'react/react-in-jsx-scope': 0,
+      'max-len': ['error', { code: 100 }],
+      'react/no-unstable-nested-components': 0,
+    },
 };
