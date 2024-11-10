00a1973038baeb963bbe32ad424fccb40f89a2da
Ever Uriel Garcia <urielcookies@outlook.com>
2024-11-10 16:58:23 -0700

    Add authentication check in Navigator: Implement AsyncStorage to manage user credentials and conditionally render login or main tabs

--- src/Components/Navigator/Navigator.tsx
+++ src/Components/Navigator/Navigator.tsx
@@ -1,19 +1,46 @@
-import { ComponentType } from 'react';
+import { ComponentType, useEffect, useState } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useTheme } from 'react-native-paper';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import Browse from '../../Views/Browse/Browse';
-import Home from '../../Views/Home/Home';
-import Menu from '../../Views/Menu/Menu';
-import Tools from '../../Views/Tools/Tools';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import SearchList from '../SearchList/SearchList';
 import TestScreen from '../TestScreen';
 import PassCodeContent from '../PassCodeContent/PassCodeContent';
 import PassCodeContentEditor from '../PassCodeContentEditor/PassCodeContentEditor';
+import Browse from '../../Views/Browse/Browse';
+import Home from '../../Views/Home/Home';
+import Menu from '../../Views/Menu/Menu';
+import Tools from '../../Views/Tools/Tools';
+import Login from '../../Views/Credentials/Login';
+import Registration from '../../Views/Credentials/Registration';
+
 
 const Navigator = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const Tab = createBottomTabNavigator();
+  const Stack = createNativeStackNavigator();
+
+  // AsyncStorage.setItem('email', 'urielcookies@outlook');
+  // AsyncStorage.setItem('password', 'Mercerst.13');
+
+  useEffect(() => {
+    const checkCredentials = async () => {
+      const email = await AsyncStorage.getItem('email');
+      const password = await AsyncStorage.getItem('password');
+      // await AsyncStorage.removeItem('email');
+      // await AsyncStorage.removeItem('password');
+      console.log({ email, password });
+
+      if (email && password) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+    };
+    checkCredentials();
+  }, []);
 
   function MyTabs() {
     const { colors } = useTheme();
@@ -83,10 +110,14 @@ const Navigator = () => {
     );
   }
 
-  const Stack = createNativeStackNavigator();
+  console.log('isAuthenticated', isAuthenticated);
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Tabs">
+    <Stack.Navigator
+      screenOptions={{ headerShown: false }}
+      initialRouteName={isAuthenticated ? 'Tabs' : 'login'}>
       <Stack.Screen name="searchList" component={SearchList} />
+      <Stack.Screen name="login" component={Login} />
+      <Stack.Screen name="registration" component={Registration} />
       <Stack.Screen name="Tabs" component={MyTabs} />
       <Stack.Screen name="TestScreen" component={TestScreen} />
       {/* Screen takes props */}
