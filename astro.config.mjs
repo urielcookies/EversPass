435850d7665e28ed800e877708c90418e3adb8b1
Ever Uriel Garcia <urielcookies@outlook.com>
2025-06-06 23:10:44 -0600

    Astro Tailwind added, removed Vites version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
+import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [tailwind(), react()]
 });
